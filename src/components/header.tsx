7616bd6ed62f679fd8a1a5030ee1121977043ac7
Matheus Oliveira <matheus.skm@hotmail.com>
2024-09-04 15:45:02 -0300

    refactor: changed img src

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export function Header() {
    return (
       <header className="w-full border-b-2 border-gray-200 p-4">
          <img
-            src="public/assets/img-idor-azul.png"
+            src="assets/img-idor-azul.png"
             alt="idor logo"
             className="h-20 w-24"
          />
