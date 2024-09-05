44d593637e3027a912719f6ded54dd20cea0ce10
Matheus Oliveira <matheus.skm@hotmail.com>
2024-09-05 10:24:13 -0300

    chore: added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-colors: {
-	"primary": "#1F369A"
+   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+   theme: {
+      extend: {
+         colors: {
+            primary: '#1F369A',
+            link: '#1F369A',
+            linkHover: '#0F448A',
+            linkActive: '#0C356A',
+            linkVisited: '#711DB0',
+         },
+      },
+   },
+   plugins: [],
 }
-},
-  },
-  plugins: [],
-};
