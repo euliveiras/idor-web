70a13c3e2928dcdefa8e4a01dbe3e5ba4868da81
Matheus Oliveira <matheus.skm@hotmail.com>
2024-09-05 08:56:06 -0300

    style: changed container padding

--- src/routes/sign-up.tsx
+++ src/routes/sign-up.tsx
@@ -7,7 +7,7 @@ export default function SignUp() {
          <Header className="flex-initial" />
          <main className="flex w-full flex-1 flex-col items-center gap-4">
             <Title title="Cadastro" />
-            <div className="flex h-full max-h-80 w-full max-w-xs flex-col items-center p-4 shadow shadow-slate-300">
+            <div className="flex h-full max-h-80 w-full max-w-xs flex-col items-center pt-12 shadow shadow-slate-300">
                <p className="text-xl font-semibold">Entrar com</p>
             </div>
          </main>
