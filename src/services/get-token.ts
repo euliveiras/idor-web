ef534e3af5d88eb055873003235f808c45b02e7f
Matheus Oliveira <matheus.skm@hotmail.com>
2024-09-10 09:12:50 -0300

    refactor: added new Url call

--- src/services/get-token.ts
+++ src/services/get-token.ts
@@ -3,8 +3,11 @@ import { getConfig } from '../configs/api'
 export async function getToken(token: string) {
    try {
       const { baseUrl } = getConfig()
-      const params = new URLSearchParams({ code: token })
-      const res = await fetch(baseUrl + params)
+		const url = new URL(baseUrl + "/auth/getAToken")
+
+		url.searchParams.set("code", token)
+
+      const res = await fetch(url)
 
       if (res.status >= 200 && res.status < 300) {
          return { data: await res.json() }
