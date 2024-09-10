3bcaaf3891d4b4f0ef8a8b201e6a934c06d45c53
Matheus Oliveira <matheus.skm@hotmail.com>
2024-09-10 08:57:32 -0300

    refactor: added loader to GetToken page

--- src/routes/get-token.tsx
+++ src/routes/get-token.tsx
@@ -1,3 +1,15 @@
+import { useLoaderData } from 'react-router-dom'
+
+export async function getTokenLoader() {
+   const data = null,
+      error = null
+
+   return { data, error }
+}
+
 export default function GetToken() {
+   const { data, error } = useLoaderData() as Awaited<
+      ReturnType<typeof getTokenLoader>
+   >
    return <h1>Loading...</h1>
 }
